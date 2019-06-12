711f89b2422b441ce59f0fe95ac4210c9d215cf6
ayd0122344 <ycvs210@gmail.com>
2019-06-12 15:22:10 +0800

    2-1.js

---
+++ 習題/2-1.js
@@ -0,0 +1,7 @@
+//計算10!
+var sum=1;
+for(var i=1; i<=10;i++)
+{
+    sum=sum*i;
+}
+console.log("The answer is",sum);
---
+++ 習題/2-2.js
@@ -0,0 +1,8 @@
+//九九乘法表
+for(var i=1;i<10;i++)
+{
+    for(var j=1;j<10;j++)
+    {
+        console.log("%d * %d =%d ",i,j,i*j);
+    }
+}
